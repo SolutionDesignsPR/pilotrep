89ff5ec82a6668a8332ff20bcbefcbf859c7d41e
SolutionDesignsPR <solutiondesigns@gmail.com>
2026-06-24 18:32:21 -0400

    Update submit-rep.js

--- netlify/functions/submit-rep.js
+++ netlify/functions/submit-rep.js
@@ -1,10 +1,1 @@
-function getSession(cookieHeader) {
-  if (!cookieHeader) return null;
-  const match = cookieHeader.match(/pilotrep_session=([^;]+)/);
-  if (!match) return null;
-  try {
-    return JSON.parse(Buffer.from(match[1], 'base64').toString('utf8'));
-  } catch {
-    return null;
-  }
-}
+exports.handler = async (event) => {
